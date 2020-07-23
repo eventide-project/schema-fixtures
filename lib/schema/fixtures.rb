45778feaccd4f54870221566dac6b0c63d25a252
Scott Bellware <sbellware@gmail.com>
2020-07-23 17:49:03 -0500

    TestBench detail shim is removed

--- lib/schema/fixtures.rb
+++ lib/schema/fixtures.rb
@@ -2,7 +2,6 @@ require 'test_bench/fixture'
 
 require 'schema'
 
-require 'schema/fixtures/test_bench_detail'
 require 'schema/fixtures/attribute'
 require 'schema/fixtures/equality'
 require 'schema/fixtures/assignment'
