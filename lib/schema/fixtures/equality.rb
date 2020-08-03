a06b27925c0750bbd502c9cc2610b67529e94fee
Scott Bellware <sbellware@gmail.com>
2020-08-03 11:12:08 -0500

    Classes are still compared when title context is not printed

--- lib/schema/fixtures/equality.rb
+++ lib/schema/fixtures/equality.rb
@@ -23,6 +23,14 @@ module Schema
         @attributes_context_name ||= 'Attributes'
       end
 
+      def control_class
+        comparison.control_class
+      end
+
+      def compare_class
+        comparison.compare_class
+      end
+
       initializer :comparison, na(:ignore_class), na(:print_title_context), na(:title_context_name), na(:attributes_context_name)
 
       def self.build(control, compare, attribute_names=nil, ignore_class: nil, print_title_context: nil, title_context_name: nil, attributes_context_name: nil)
@@ -30,45 +38,42 @@ module Schema
         new(comparison, ignore_class, print_title_context, title_context_name, attributes_context_name)
       end
 
-      def call()
-        control_class = comparison.control_class
-        compare_class = comparison.compare_class
-
-        test_attributes = proc do
-          context attributes_context_name do
-            comparison.entries.each do |entry|
-              printed_attribute_name = Attribute.printed_name(entry)
-
-              control_attribute_value = entry.control_value
-              compare_attribute_value = entry.compare_value
-
-              test printed_attribute_name do
-                detail "#{control_class.name.split('::').last} Value: #{control_attribute_value.inspect}"
-                detail "#{compare_class.name.split('::').last} Value: #{compare_attribute_value.inspect}"
-
-                assert(compare_attribute_value == control_attribute_value)
-              end
-            end
-          end
-        end
-
+      def call
         if print_title_context?
           context "#{title_context_name}" do
             detail "Control Class: #{control_class.name}"
             detail "Compare Class: #{compare_class.name}"
 
-            if not ignore_class?
-              test "Classes are the same" do
-                assert(control_class == compare_class)
-              end
-            else
-              detail 'Class comparison is ignored'
-            end
-
-            test_attributes.()
+            call!
           end
         else
-          test_attributes.()
+          call!
+        end
+      end
+
+      def call!
+        if not ignore_class?
+          test "Classes are the same" do
+            assert(control_class == compare_class)
+          end
+        else
+          detail 'Class comparison is ignored'
+        end
+
+        context attributes_context_name do
+          comparison.entries.each do |entry|
+            printed_attribute_name = Attribute.printed_name(entry)
+
+            control_attribute_value = entry.control_value
+            compare_attribute_value = entry.compare_value
+
+            test printed_attribute_name do
+              detail "#{control_class.name.split('::').last} Value: #{control_attribute_value.inspect}"
+              detail "#{compare_class.name.split('::').last} Value: #{compare_attribute_value.inspect}"
+
+              assert(compare_attribute_value == control_attribute_value)
+            end
+          end
         end
       end
     end
