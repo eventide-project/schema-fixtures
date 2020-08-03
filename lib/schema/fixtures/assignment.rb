7b08f8e36d57b71035ebcd4f516e85fd7a4b3b97
Scott Bellware <sbellware@gmail.com>
2020-08-03 11:20:27 -0500

    Assignment fixture respects standards for shared implementation

--- lib/schema/fixtures/assignment.rb
+++ lib/schema/fixtures/assignment.rb
@@ -19,6 +19,10 @@ module Schema
         @attributes_context_name ||= 'Attributes'
       end
 
+      def schema_class
+        comparison.control_class
+      end
+
       initializer :comparison, na(:print_title_context), na(:title_context_name), na(:attributes_context_name)
 
       def self.build(compare, attribute_names=nil, print_title_context: nil, title_context_name: nil, attributes_context_name: nil)
@@ -27,33 +31,31 @@ module Schema
         new(comparison, print_title_context, title_context_name, attributes_context_name)
       end
 
-      def call()
-        schema_class = comparison.control_class
-
-        test_attributes = proc do
-          context attributes_context_name do
-            comparison.entries.each do |entry|
-              default_attribute_value = entry.control_value
-              attribute_value = entry.compare_value
-
-              test "#{entry.compare_name}" do
-                detail "Default Value: #{default_attribute_value.inspect}"
-                detail "Assigned Value: #{attribute_value.inspect}"
-
-                refute(attribute_value == default_attribute_value)
-              end
-            end
-          end
-        end
-
+      def call
         if print_title_context?
           context "#{title_context_name}" do
             detail "Class: #{schema_class.name}"
 
-            test_attributes.()
+            call!
           end
         else
-          test_attributes.()
+          call!
+        end
+      end
+
+      def call!
+        context attributes_context_name do
+          comparison.entries.each do |entry|
+            default_attribute_value = entry.control_value
+            attribute_value = entry.compare_value
+
+            test "#{entry.compare_name}" do
+              detail "Default Value: #{default_attribute_value.inspect}"
+              detail "Assigned Value: #{attribute_value.inspect}"
+
+              refute(attribute_value == default_attribute_value)
+            end
+          end
         end
       end
     end
